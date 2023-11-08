26554d2135f4916f532ab23bf93a02a7c0f8d2d7
Aleksandar Stojanov <me@fnd.works>
2023-11-08 17:43:28 +0100

    tidy up a bit
    Signed-off-by: Aleksandar Stojanov <me@fnd.works>

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -1,5 +1,4 @@
 import * as util from 'util'
-import * as core from '@actions/core'
 import {
   run,
   installPlugin,
@@ -7,12 +6,14 @@ import {
   isPluginInstalled
 } from '../src/index'
 import * as indexModule from '../src/index'
-import { expect } from '@jest/globals'
+import * as core from '@actions/core'
+import { simpleGit } from 'simple-git'
+import { jest } from '@jest/globals'
 
 jest.mock('@actions/core')
 jest.mock('util')
-jest.mock('simple-git')
 jest.mock('child_process')
+jest.mock('simple-git')
 
 describe('isHelmInstalled', () => {
   it('should return true when Helm is installed', async () => {
@@ -81,7 +82,7 @@ describe('installPlugin', () => {
 
     await installPlugin()
 
-    // expect(core.setFailed).not.toHaveBeenCalled()
+    expect(core.setFailed).not.toHaveBeenCalled()
     expect(core.info).toHaveBeenCalledTimes(1)
   })
 
@@ -95,76 +96,7 @@ describe('installPlugin', () => {
 
     await installPlugin()
 
-    // expect(core.setFailed).not.toHaveBeenCalled()
+    expect(core.setFailed).not.toHaveBeenCalled()
     expect(core.info).toHaveBeenCalledTimes(1)
   })
 })
-
-// describe('run', () => {
-//   beforeEach(() => {
-//     jest.clearAllMocks();
-//   });
-
-//   it('should run successfully when everything is set up correctly', async () => {
-//     // Mock core.getInput to return the expected values
-//     jest.spyOn(core, 'getInput').mockImplementation((name) => {
-//       switch (name) {
-//         case 'input':
-//           return '__tests__/values.yaml';
-//         case 'draft':
-//           return '2020';
-//         case 'output':
-//           return 'values-schema.json';
-//         case 'git-push':
-//           return 'false';
-//         case 'git-push-user-name':
-//           return 'github-actions[bot]';
-//         case 'git-push-user-email':
-//           return 'github-actions[bot]@users.noreply.github.com';
-//         case 'git-commit-message':
-//           return 'update values.schema.json';
-//         case 'fail-on-diff':
-//           return 'true';
-//         default:
-//           return '';
-//       }
-//     });
-
-//     // Mock any other necessary dependencies (e.g., simple-git) with jest.fn() as needed
-
-//     // Mock util.promisify to resolve without errors
-//     jest.spyOn(util, 'promisify').mockImplementation((fn) => {
-//       return (...args: any[]) => {
-//         return Promise.resolve('Command output');
-//       };
-//     });
-
-//     await run();
-
-//     // Add your assertions here to check the expected behavior
-
-//     expect(core.info).toHaveBeenCalledTimes(2); // Check info messages
-//     expect(core.info).toHaveBeenCalledWith('Merged data saved to ${output}');
-//     // ... add more assertions based on the expected behavior
-//   });
-
-//   it('should set a failure message if an error occurs during helm schema command execution', async () => {
-//     // Mock core.getInput to return the expected values
-
-//     // Mock util.promisify to reject with an error
-//     jest.spyOn(util, 'promisify').mockImplementation((fn) => {
-//       return (...args: any[]) => {
-//         return Promise.reject(new Error('Command failed'));
-//       };
-//     });
-
-//     await run();
-
-//     // Add your assertions here to check the expected behavior
-//     expect(core.setFailed).toHaveBeenCalledWith('Error running helm schema command');
-//     expect(core.info).not.toHaveBeenCalled();
-//     // ... add more assertions based on the expected behavior
-//   });
-
-//   // Add more test cases as needed to cover different scenarios
-// });
