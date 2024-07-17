da4ac94944081a27c99d436fd7b86c42b6f6057b
Aleksandar Stojanov <me@fnd.works>
2024-07-17 19:06:44 +0200

    bump plugin verision
    Signed-off-by: Aleksandar Stojanov <me@fnd.works>

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import * as core from '@actions/core'
 import * as exec from '@actions/exec'
 import { simpleGit } from 'simple-git'
 
-const version = 'v1.5.1'
+const version = 'v1.5.2'
 
 /**
  * The main function for the action.
