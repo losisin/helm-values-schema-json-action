3915dc0a37bdfeb4b2dbc0e00d93457bc30b29d4
Aleksandar Stojanov <me@fnd.works>
2025-06-07 13:28:20 +0200

    release v1.7.1
    Signed-off-by: Aleksandar Stojanov <me@fnd.works>

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { simpleGit } from 'simple-git'
 import { parse } from 'yaml'
 import * as fs from 'fs/promises'
 
-const version = 'v1.9.0'
+const version = 'v1.9.2'
 
 interface SchemaConfig {
   input?: string[]
